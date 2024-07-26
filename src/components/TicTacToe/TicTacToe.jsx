6e1b30ea083fcef7083f6a5b2da92100a6c12c3b
Soumyadeep3004 <soumyadeepsingha10@gmail.com>
2024-07-26 21:53:27 +0530

    testing changes

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -81,6 +81,7 @@ export default function TicTacToe() {
         boxes.map((box)=>(
             box.current.innerHTML=""
         ))
+        console.log("hello")
     }
 
     const handleClick = (e)=>{
